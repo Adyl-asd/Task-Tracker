30ea054e346cf3133fca92c29fde7183ad71fadd
Адылжан Тухтамов <adyl0810@gmail.com>
2022-04-09 21:43:57 +0600

    #6 Created UserController with GET/POST methods. Templates are not ready yet.  - GET method for viewing profile page  - GET method for viewing sign up page  - GET method for viewing login page  - POST method for registering

---
+++ src/main/java/com/example/tasktracker/common_web/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.example.tasktracker.common_web.controller;
+
+import com.example.tasktracker.common_service.UserService;
+import com.example.tasktracker.dto.create.UserRegisterDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
+@Controller
+@RequestMapping
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/profile")
+    public String userProfile(Model model, Principal principal) {
+        var user = userService.getByEmail(principal.getName());
+        model.addAttribute("user", user);
+        return "profile";
+    }
+
+    @GetMapping("/register")
+    public String registerUser(Model model) {
+        if (!model.containsAttribute("user"))
+            model.addAttribute("userForm", new UserRegisterDTO());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@Valid UserRegisterDTO form,
+                               BindingResult validationResult,
+                               RedirectAttributes attributes) {
+        attributes.addFlashAttribute("userForm", form);
+        if (validationResult.hasFieldErrors()) {
+            attributes.addFlashAttribute("errors", validationResult.getFieldErrors());
+            return "redirect:/register";
+        }
+        userService.register(form);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam(required = false, defaultValue = "false") Boolean error, Model model) {
+        model.addAttribute("error", error);
+        return "login";
+    }
+}
