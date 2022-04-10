fc368c42aa716baac216957dce8f47ae3f76a913
Адылжан Тухтамов <adyl0810@gmail.com>
2022-04-10 19:45:27 +0600

    #15 Added missed client side validation

--- src/main/resources/static/js/project_update.js
+++ src/main/resources/static/js/project_update.js
@@ -4,7 +4,10 @@ $(document).ready(function () {
         // Check if all inputs are full before ajax request
         if (!$('#inputName').val() || !$('#inputStartDate').val() || !$('#inputEndDate').val() || !$('#inputPriority').val() || !$('#inputStatus').val()) {
             alert('Filling all fields is required!')
-        } else {
+        } if ($('#inputPriority').val() > 3 || $('#inputPriority').val() < 1) {
+            alert('Priority can be only from 1 to 3')
+        }
+        else {
             // Prevents multiple button clicks firing new events
             e.stopImmediatePropagation();
             e.preventDefault();
